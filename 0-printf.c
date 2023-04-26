656db77e3e82925419cfff45951dd24990a78176
edake <edisondake@gmail.com>
2023-04-26 01:00:21 +0000

    second

--- 0-printf.c
+++ 0-printf.c
@@ -18,7 +18,6 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-
 			if (*format == 'c')
 			{
 				char c = va_arg(arguments, int);
@@ -28,7 +27,6 @@ int _printf(const char *format, ...)
 			else if (*format == 's')
 			{
 				char *s = va_arg(arguments, char *);
-
 				while (*s)
 				{
 					putchar(*s);
@@ -41,17 +39,60 @@ int _printf(const char *format, ...)
 				putchar('%');
 				printed++;
 			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(arguments, int);
+				if (num < 0)
+				{
+					num = -num;
+					putchar('-');
+					printed++;
+				}
+				printed += print_number(num);
+			}
 		}
 		else
 		{
 			putchar(*format);
 			printed++;
 		}
-
 		format++;
 	}
 
 	va_end(arguments);
-
 	return (printed);
+}
+
+/**
+ * print_number - Print a number.
+ * @n: Number to print.
+ *
+ * Return: Number of digits printed.
+ */
+int print_number(int n)
+{
+	int digits = 0;
+	int i;
+	int divisor = 1000000000;
+
+	if (n == 0)
+	{
+		putchar('0');
+		return (1);
+	}
+
+	while (n / divisor == 0 && divisor > 1)
+		divisor /= 10;
+
+	for (i = divisor; i >= 1; i /= 10)
+	{
+		if (n / i != 0 || digits > 0 || i == 1)
+		{
+			putchar('0' + n / i);
+			n %= i;
+			digits++;
+		}
+	}
+
+	return (digits);
 }
