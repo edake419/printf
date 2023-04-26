7e7329cd9b6e626c549dda2f549afea98ef6ba6d
edake <edisondake@gmail.com>
2023-04-26 01:03:53 +0000

    second

--- 0-printf.c
+++ 0-printf.c
@@ -62,37 +62,3 @@ int _printf(const char *format, ...)
 	va_end(arguments);
 	return (printed);
 }
-
-/**
- * print_number - Print a number.
- * @n: Number to print.
- *
- * Return: Number of digits printed.
- */
-int print_number(int n)
-{
-	int digits = 0;
-	int i;
-	int divisor = 1000000000;
-
-	if (n == 0)
-	{
-		putchar('0');
-		return (1);
-	}
-
-	while (n / divisor == 0 && divisor > 1)
-		divisor /= 10;
-
-	for (i = divisor; i >= 1; i /= 10)
-	{
-		if (n / i != 0 || digits > 0 || i == 1)
-		{
-			putchar('0' + n / i);
-			n %= i;
-			digits++;
-		}
-	}
-
-	return (digits);
-}
